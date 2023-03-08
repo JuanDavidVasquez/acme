338ac4a0b9ba525e260c87125cb3568afbc4f5df
JuanDavidVasquez <67479047+JuanDavidVasquez@users.noreply.github.com>
2023-03-08 06:01:59 -0500

    601

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Conductor } from 'src/app/models/conductor';
+import { ConductorService } from 'src/app/service/conductor.service';
+import { GLOBAL } from 'src/app/service/global';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'acme';
+
+  constructor(
+    private _route:ActivatedRoute,
+    private _router:Router,
+  ) {
+
+
+   }
+
+   ngOnInit(): void {}
+  conductor(){
+    this._router.navigate(['/conductor']);
+  }
+  vehiculo(){
+  this._router.navigate(['/vehiculo']);
+  }
+  propietario(){
+  this._router.navigate(['/propietario']);
+  }
 }
